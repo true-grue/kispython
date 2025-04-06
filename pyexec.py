21209a4f8734e39f2c5e55a9b88271c225051a89
Peter Sovietov <peter.sovietov@gmail.com>
2025-04-06 14:32:00 +0300

    pyexec: functions

--- pyexec.py
+++ pyexec.py
@@ -1,14 +1,8 @@
 import ast
 
 
-def PyObject(ty, val):
-    return {'type': ty, 'value': val}
-
-
-Type = PyObject(None, 'type')
-Type['type'] = Type
-Int = PyObject(Type, 'int')
-Builtin = PyObject(Type, 'builtin_function_or_method')
+def make_object(ty, val):
+    return {'__class__': ty, 'value': val}
 
 
 def load_name(frame, name):
@@ -30,20 +24,12 @@ def exec_expr(frame, tree):
         case ast.Name(name):
             return load_name(frame, name)
         case ast.Constant(int(value)):
-            return PyObject(Int, value)
-        case ast.Call(ast.Name(name), [arg]):
-            func = load_name(frame, name)['value']
-            return func(exec_expr(frame, arg))
-        case _:
-            raise SyntaxError
-
-
-def exec_stmt(frame, tree):
-    match tree:
-        case ast.Assign([name], expr):
-            store_name(frame, name.id, exec_expr(frame, expr))
-        case ast.Expr(expr):
-            exec_expr(frame, expr)
+            return make_object(Int, value)
+        case ast.Call(ast.Name(name), args):
+            obj = load_name(frame, name)
+            ty = obj['__class__']
+            args = (exec_expr(frame, arg) for arg in args)
+            return ty['__call__'](frame, obj, *args)
         case _:
             raise SyntaxError
 
@@ -53,39 +39,81 @@ def exec_block(frame, tree):
         exec_stmt(frame, stmt)
 
 
-def exec_module(frame, tree):
+def exec_stmt(frame, tree):
     match tree:
-        case ast.Module(block):
-            exec_block(frame, block)
+        case ast.FunctionDef(name, args, body):
+            func = make_object(Function, (args.args, body))
+            store_name(frame, name, func)
+        case ast.Assign([ast.Name(name)], expr):
+            store_name(frame, name, exec_expr(frame, expr))
+        case ast.Expr(expr):
+            exec_expr(frame, expr)
         case _:
             raise SyntaxError
 
 
-def print_func(obj):
-    print(obj['value'])
+def exec_module(frame, tree):
+    match tree:
+        case ast.Module(block):
+            new_frame = frame | {'locals': frame['globals']}
+            exec_block(new_frame, block)
+        case _:
+            raise SyntaxError
 
 
-def type_func(obj):
-    return obj['type']
+def call_func(frame, obj, *args):
+    params, body = obj['value']
+    locs = {p.arg: a for p, a in zip(params, args)}
+    exec_block(frame | {'locals': locs}, body)
+
+
+def call_builtin(frame, obj, *args):
+    return obj['value'](*args)
+
+
+Type = make_object(None, '__class__')
+Type['__class__'] = Type
+Int = make_object(Type, 'int')
+Str = make_object(Type, 'str')
+
+
+Function = {
+    '__call__': call_func,
+    '__name__': make_object(Str, 'function')
+}
+
+
+Builtin = {
+    '__call__': call_builtin,
+    '__name__': make_object(Str, 'builtin_function_or_method')
+}
+
+
+def builtin_type(obj):
+    return obj['__class__']['__name__']
+
+
+def builtin_print(*args):
+    print(*(arg['value'] for arg in args))
 
 
 builtins = {
-    'print': PyObject(Builtin, print_func),
-    'type': PyObject(Builtin, type_func)
+    'type': make_object(Builtin, builtin_type),
+    'print': make_object(Builtin, builtin_print)
 }
 
-globs = {}
-
-Frame = {
-    'locals': globs,
-    'globals': globs,
-    'builtins': builtins
-}
 
 src = '''
-x = 42
-print(type(x))
-print(print)
+G = 1
+def f(a):
+    G = 2
+    print(G, a)
+
+f(1)
+f(2)
+print(G)
+print(type(f))
 '''
 
-exec_module(Frame, ast.parse(src))
+frame = {'builtins': builtins, 'globals': {}}
+exec_module(frame, ast.parse(src))
